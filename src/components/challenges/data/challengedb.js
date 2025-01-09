6be0fdde874ac56bd135e5071130bfffd8111bca
MalwareCube <malwarecube@gmail.com>
2025-01-09 09:56:44 -0500

    added splunk challenge

--- src/components/challenges/data/challengedb.js
+++ src/components/challenges/data/challengedb.js
@@ -37,7 +37,7 @@ const challengedb = [
     },
     {
       id: "SOC-2258",
-      title: "Splunk",
+      title: "Corporate Web Server Compromise",
       category: "SIEM Investigation",
       data: require('./SOC-2258.json'),
     },
